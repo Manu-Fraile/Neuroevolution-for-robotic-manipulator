8a19a51dcf2ebcd0f929131a9f4997f12f1ea9dc
Simone Morettini <s.morettini.cs@gmail.com>
2020-10-30 12:04:57 +0100

    Scheleton of project

---
+++ direct_kinematic.py
@@ -0,0 +1,8 @@
+
+class DirectKinematic(object):
+
+    def __init__(self, parameters={}):
+        return
+
+    def evaluate(self, array_of_ee_joint_coordinates):
+        return [x,y,z]
---
+++ fitness_functions.py
@@ -0,0 +1,21 @@
+
+
+from direct_kinematic import DirectKinematic
+
+class FitnessFunctions(object):
+
+    def __init__(self, parameters={}):
+        return
+
+    def evaluate_energy(self, array_of_joints_coordinates):
+        return value
+
+    def evaluate_operation_time(self, array_of_joints_coordinates):
+        return value
+
+    def evaluate_rotations(self, array_of_joints_coordinates):
+        return value
+
+    def evaluate_position_accuracy(self, array_of_joints_coordinates, array_of_objective_ee):
+
+        return value
