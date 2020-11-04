89835f001ae12606477efb622351544892123194
Manu-Fraile <manu.fra.ro@gmail.com>
2020-11-04 18:44:14 +0100

    first approach to DK

--- direct_kinematic.py
+++ direct_kinematic.py
@@ -1,8 +1,93 @@
 
+import math as m
+import numpy as np
+from numpy import cos, sin
+
+
 class DirectKinematic(object):
 
     def __init__(self, parameters={}):
         return
 
-    def evaluate(self, array_of_ee_joint_coordinates):
-        return [x,y,z]
+    def evaluate(self, array_of_joints_coordinates):
+
+        o = array_of_joints_coordinates[0]
+        t = array_of_joints_coordinates[1]
+        s = array_of_joints_coordinates[2]
+        f = array_of_joints_coordinates[3]
+        r = array_of_joints_coordinates[4]
+        e = array_of_joints_coordinates[5]
+
+        transformMat = transformM(o, t, s, f, r, e)
+
+        efMatrix = np.array([[0.3],
+                             [0],
+                             [0.1],
+                             [1]])
+
+        efPosition = np.dot(transformMat, efMatrix)
+
+        return efPosition
+
+
+# omega: -180 ; 180
+# theta: -70 ; 70
+# psi: -28 ; 105
+# fi: -300 ; 300
+# ro: -120 ; 120
+# epsilon: -300 ; 300
+def transformM(o, t, s, f, r, e, R1=0.188, R2=1.175, R3=1.3, R4=0.2):  # omega, theta, psi, fi, ro, epsilon
+
+    A = np.array([[cos(o), -sin(o), 0, 0],
+                  [sin(o), cos(o), 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]])
+
+    B = np.array([[1, 0, 0, R1],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]])
+
+    C = np.array([[cos(t), 0, sin(t), 0],
+                  [0, 1, 0, 0],
+                  [-sin(t), 0, cos(t), 0],
+                  [0, 0, 0, 1]])
+
+    D = np.array([[1, 0, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, R2],
+                  [0, 0, 0, 1]])
+
+    E = np.array([[cos(s), 0, sin(s), 0],
+                  [0, 1, 0, 0],
+                  [-sin(s), 0, cos(s), 0],
+                  [0, 0, 0, 1]])
+
+    F = np.array([[1, 0, 0, 0],
+                  [0, cos(f), -sin(f), 0],
+                  [0, sin(f), cos(f), 0],
+                  [0, 0, 0, 1]])
+
+    G = np.array([[1, 0, 0, R3],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]])
+
+    H = np.array([[cos(r), 0, sin(r), 0],
+                  [0, 1, 0, 0],
+                  [-sin(r), 0, cos(r), 0],
+                  [0, 0, 0, 1]])
+
+    I = np.array([[1, 0, 0, R4],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]])
+
+    J = np.array([[1, 0, 0, 0],
+                  [0, cos(e), -sin(e), 0],
+                  [0, sin(e), cos(e), 0],
+                  [0, 0, 0, 1]])
+
+    TM = A * B * C * D * E * F * G * H * I * J
+
+    return TM
--- fitness_functions.py
+++ fitness_functions.py
@@ -1,6 +1,9 @@
 
 import math
+import numpy as np
+from numpy import sqrt
 from direct_kinematic import DirectKinematic
+
 
 class FitnessFunctions(object):
 
@@ -11,32 +14,49 @@ class FitnessFunctions(object):
 
     def evaluate_energy(self, array_of_joints_coordinates):
         total_energy=0
-        for joint in range(0,6):
+        for joint in range(0, 6):
             degrees_tmp=0
-            for coo in range(0,10):
+            for coo in range(0, 10):
                 # rotation=|joint_b-joint_a|
                 degrees_tmp=degrees_tmp+abs(array_of_joints_coordinates[(coo+1)%10][joint]-array_of_joints_coordinates[coo][joint])
             total_energy=total_energy+math.degrees(degrees_tmp)*self.energy_constants[joint]
         return total_energy
 
     def evaluate_operation_time(self, array_of_joints_coordinates):
-        total_operation_time=0
-        for coo in range(0,10):
+        total_operation_time = 0
+        for coo in range(0, 10):
             degrees=[]
-            for joint in range(0,6):
+            for joint in range(0, 6):
                 # rotation=|joint_b-joint_a|
                 degrees.append(abs(array_of_joints_coordinates[(coo+1)%10][joint]-array_of_joints_coordinates[coo][joint]))
             total_operation_time=total_operation_time+max(degrees)*self.energy_constants[degrees.index(max(degrees))]
         return total_operation_time
 
     def evaluate_rotations(self, array_of_joints_coordinates):
-        total_rotations=0
-        for joint in range(0,6):
-            for coo in range(0,10):
+        total_rotations = 0
+        for joint in range(0, 6):
+            for coo in range(0, 10):
                 # rotation=|joint_b-joint_a|
                 total_rotations=total_rotations+abs(array_of_joints_coordinates[(coo+1)%10][joint]-array_of_joints_coordinates[coo][joint])
         return total_rotations
 
-    def evaluate_position_accuracy(self, array_of_joints_coordinates, array_of_objective_ee):
+    def evaluate_position_accuracy(self, array_of_joints_coordinates, points):
 
-        return value
+        directKinematics = DirectKinematic()
+
+        total_accuracy = 0
+
+        for i in range(0, 10):
+
+            homogenousPred = directKinematics.evaluate(array_of_joints_coordinates[i])    # This is in homogenous coordinates
+            predictedPosition = np.array([[homogenousPred[0][0] / homogenousPred[3][0]],
+                                          [homogenousPred[1][0] / homogenousPred[3][0]],
+                                          [homogenousPred[2][0] / homogenousPred[3][0]]])       #This is cartesian coordinates
+
+            x_diff = (points[i][0] - predictedPosition[0])**2
+            y_diff = (points[i][1] - predictedPosition[1])**2
+            z_diff = (points[i][2] - predictedPosition[2])**2
+            differences = x_diff + y_diff + z_diff
+            total_accuracy += sqrt(differences)
+
+        return total_accuracy[0]
